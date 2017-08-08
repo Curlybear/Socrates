0d52799b981b4b3c8f521efde1502f6132998d5a
Erwan Flohic <eflohic@ulb.ac.be>
2017-08-09 00:57:01 +0200

    Rework the help command

--- bot.py
+++ bot.py
@@ -39,9 +39,9 @@ apiKey= config['DEFAULT']['api_key']
 #Discord.py bot setup
 description = 'Available commands \n !mpp [country name] - Returns a list of mpps for the specified country \n !jobs [number|country name]- Returns the top jobs overall or for a specific country \n !cinfo [country name] - Returns a list of information for the specified country \n !user [username|userid] - Return the information regarding a specified user \n !history (cs|name|mu|party) [username|userid] - Return a specific history of information for a specified user \n !battle (info|co) [battleid] - Return the information regarding a specified battle\n\nMore information at https://curlybear.eu/socrates \nPowered by erepublik-deutschland.de'
 
-help_attrs = dict(hidden=True)
+bot = commands.Bot(command_prefix='!', description=description)
 
-bot = commands.Bot(command_prefix='!', description=description, pm_help=None, help_attrs=help_attrs)
+bot.remove_command("help")
 
 #Helper functions
 def is_number(s):
@@ -86,6 +86,23 @@ async def on_ready():
     logger.info('Bot started as ' + bot.user.name)
     await bot.change_presence(game=discord.Game(name='eRepublik'))
 
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    logger.info('!help - User: ' + str(ctx.message.author))
+    em = discord.Embed(title='Help', description='Available commands', colour=0x0053A9)
+    em.add_field(name="!mpp [country name]", value="Returns a list of mpps for the specified country", inline=False)
+    em.add_field(name="!jobs [number|country name]", value="Returns the top jobs overall or for a specific country", inline=False)
+    em.add_field(name="!cinfo [country name]", value="Returns a list of information for the specified country", inline=False)
+    em.add_field(name="!user [username|userid]", value="Return the information regarding a specified user", inline=False)
+    em.add_field(name="!battle (info|co) [battleid]", value="Return the information regarding a specified battle", inline=False)
+    em.add_field(name="!history (cs|name|mu|party) [username|userid]", value="Return a specific history of information for a specified user", inline=False)
+    em.add_field(name="!convert [erepDay|date]", value="Return the conversion of the provided erepDay or date (dd/mm/yyyy)", inline=False)
+    em.set_author(name="Curlybear#1962", url='https://curlybear.eu', icon_url='https://erpk-static-avatars.s3.amazonaws.com/avatars/Citizens/2011/07/12/0a83af20636fe1ac4d01c6d132572943.png?8e8837b38eeff670f9301b95d31a47a2')
+    em.set_thumbnail(url='http://www.dipsacademy.com/images/socrates.png')
+    em.set_footer(text='Powered by http://api.erepublik-deutschland.de/')
+    await bot.send_message(ctx.message.channel, '', embed=em)
+    
 @bot.command(pass_context=True)
 async def mpp(ctx, inCountry : str):
     logger.info('!mpp ' + inCountry + ' - User: ' + str(ctx.message.author))
@@ -226,6 +243,7 @@ async def user(ctx, inValue):
 
 @bot.command(pass_context=True)
 async def ping(ctx):
+    """Pings the bot"""
     logger.info('!ping - User: ' + str(ctx.message.author))
     em = discord.Embed(title='Pong', description='pong', colour=0x3D9900)
     await bot.send_message(ctx.message.channel, '', embed=em)
