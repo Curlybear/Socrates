d97006617a5de6d9ff9295d7ba55bbaeea9740c9
Curlybear <tehcurlybear@gmail.com>
2018-07-14 14:38:09 +0200

    Setup Travis, maybe

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Socrates',
+    version='1.0',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/Curlybear/Socrates',
+    license='',
+    author='Curlybear',
+    author_email='me@curlybear.eu',
+    description='A discord bot for the erepublik web browser game'
+)
